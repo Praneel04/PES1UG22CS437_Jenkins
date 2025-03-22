270cb4d42120789baa2224237c43c354fe90376a
PRANEEL PATEL GM <117033281+Praneel04@users.noreply.github.com>
2025-03-22 22:27:42 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<<"THis is the main file"<<endl;
+  cout<<"This is newly created"<<endl;
+  cout<<"SRN:PES1UG22CS437"<<endl;
+  return 0;
+}
